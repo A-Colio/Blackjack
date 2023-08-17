7889daa8f6eda75618d5fd0de6b44f3b7fda055e
krhxsta <62493408+krhxsta@users.noreply.github.com>
2023-08-17 15:34:33 +0900

    Add files via upload

---
+++ src/cards.mjs
@@ -0,0 +1,318 @@
+const cards = {
+
+    spadeAce : {
+        name : "♠A",
+        number : [1, 11],
+        image : './assets/cardSprite/ace_of_spades.svg'
+    },
+
+    spadeTwo : {
+        name : "♠2",
+        number : 2,
+        image : './assets/cardSprite/2_of_spades.svg'
+    },    
+    
+    spadeThree : {
+        name : "♠3",
+        number : 3,
+        image : './assets/cardSprite/3_of_spades.svg'
+    },    
+    
+    spadeFour : {
+        name : "♠4",
+        number : 4,
+        image : './assets/cardSprite/4_of_spades.svg'
+    },    
+    
+    spadeFive : {
+        name : "♠5",
+        number : 5,
+        image : './assets/cardSprite/5_of_spades.svg'
+    },    
+    
+    spadeSix : {
+        name : "♠6",
+        number : 6,
+        image : './assets/cardSprite/6_of_spades.svg'
+    },    
+    
+    spadeSeven : {
+        name : "♠7",
+        number : 7,
+        image : './assets/cardSprite/7_of_spades.svg'
+    },    
+    
+    spadeEight : {
+        name : "♠8",
+        number : 8,
+        image : './assets/cardSprite/8_of_spades.svg'
+    },    
+    
+    spadeNine : {
+        name : "♠9",
+        number : 9,
+        image : './assets/cardSprite/9_of_spades.svg'
+    },    
+    
+    spadeTen : {
+        name : "♠10",
+        number : 10,
+        image : './assets/cardSprite/10_of_spades.svg'
+    },    
+    
+    spadeJack : {
+        name : "♠J",
+        number : 10,
+        image : './assets/cardSprite/jack_of_spades.svg'
+    },
+
+    spadeQueen : {
+        name : "♠Q",
+        number : 10,
+        image : './assets/cardSprite/queen_of_spades.svg'
+    },
+
+    spadeKing : {
+        name : "♠K",
+        number : 10,
+        image : './assets/cardSprite/king_of_spades.svg'
+    },
+
+    diaAce : {
+        name : "♦A",
+        number : [1, 11],
+        image : './assets/cardSprite/ace_of_diamonds.svg'
+    },
+
+    diaTwo : {
+        name : "♦2",
+        number : 2,
+        image : './assets/cardSprite/2_of_diamonds.svg'
+    },    
+    
+    diaThree : {
+        name : "♦3",
+        number : 3,
+        image : './assets/cardSprite/3_of_diamonds.svg'
+    },    
+    
+    diaFour : {
+        name : "♦4",
+        number : 4,
+        image : './assets/cardSprite/4_of_diamonds.svg'
+    },    
+    
+    diaFive : {
+        name : "♦5",
+        number : 5,
+        image : './assets/cardSprite/5_of_diamonds.svg'
+    },    
+    
+    diaSix : {
+        name : "♦6",
+        number : 6,
+        image : './assets/cardSprite/6_of_diamonds.svg'
+    },    
+    
+    diaSeven : {
+        name : "♦7",
+        number : 7,
+        image : './assets/cardSprite/7_of_diamonds.svg'
+    },    
+    
+    diaEight : {
+        name : "♦8",
+        number : 8,
+        image : './assets/cardSprite/8_of_diamonds.svg'
+    },    
+    
+    diaNine : {
+        name : "♦9",
+        number : 9,
+        image : './assets/cardSprite/9_of_diamonds.svg'
+    },    
+    
+    diaTen : {
+        name : "♦10",
+        number : 10,
+        image : './assets/cardSprite/10_of_diamonds.svg'
+    },    
+    
+    diaJack : {
+        name : "♦J",
+        number : 10,
+        image : './assets/cardSprite/jack_of_diamonds.svg'
+    },
+
+    diaQueen : {
+        name : "♦Q",
+        number : 10,
+        image : './assets/cardSprite/queen_of_diamonds.svg'
+    },
+
+    diaKing : {
+        name : "♦K",
+        number : 10,
+        image : './assets/cardSprite/king_of_diamonds.svg'
+    },
+
+    heartAce : {
+        name : "♥A",
+        number : [1, 11],
+        image : './assets/cardSprite/ace_of_hearts.svg'
+    },
+
+    heartTwo : {
+        name : "♥2",
+        number : 2,
+        image : './assets/cardSprite/2_of_hearts.svg'
+    },    
+    
+    heartThree : {
+        name : "♥3",
+        number : 3,
+        image : './assets/cardSprite/3_of_hearts.svg',
+    },    
+    
+    heartFour : {
+        name : "♥4",
+        number : 4,
+        image : './assets/cardSprite/4_of_hearts.svg'
+    },    
+    
+    heartFive : {
+        name : "♥5",
+        number : 5,
+        image : './assets/cardSprite/5_of_hearts.svg'
+    },    
+    
+    heartSix : {
+        name : "♥6",
+        number : 6,
+        image : './assets/cardSprite/6_of_hearts.svg'
+    },    
+    
+    heartSeven : {
+        name : "♥7",
+        number : 7,
+        image : './assets/cardSprite/7_of_hearts.svg'
+    },    
+    
+    heartEight : {
+        name : "♥8",
+        number : 8,
+        image : './assets/cardSprite/8_of_hearts.svg'
+    },    
+    
+    heartNine : {
+        name : "♥9",
+        number : 9,
+        image : './assets/cardSprite/9_of_hearts.svg'
+    },    
+    
+    heartTen : {
+        name : "♥10",
+        number : 10,
+        image : './assets/cardSprite/10_of_hearts.svg'
+    },    
+    
+    heartJack : {
+        name : "♥J",
+        number : 10,
+        image : './assets/cardSprite/jack_of_hearts.svg'
+    },
+
+    heartQueen : {
+        name : "♥Q",
+        number : 10,
+        image : './assets/cardSprite/queen_of_hearts.svg'
+    },
+
+    heartKing : {
+        name : "♥K",
+        number : 10,
+        image : './assets/cardSprite/king_of_hearts.svg'
+    },
+
+    clubAce : {
+        name : "♣A",
+        number : [1, 11],
+        image : './assets/cardSprite/ace_of_clubs.svg'
+    },
+
+    clubTwo : {
+        name : "♣2",
+        number : 2,
+        image : './assets/cardSprite/2_of_clubs.svg'
+    },    
+    
+    clubThree : {
+        name : "♣3",
+        number : 3,
+        image : './assets/cardSprite/3_of_clubs.svg'
+    },    
+    
+    clubFour : {
+        name : "♣4",
+        number : 4,
+        image : './assets/cardSprite/4_of_clubs.svg'
+    },    
+    
+    clubFive : {
+        name : "♣5",
+        number : 5,
+        image : './assets/cardSprite/5_of_clubs.svg'
+    },    
+    
+    clubSix : {
+        name : "♣6",
+        number : 6,
+        image : './assets/cardSprite/6_of_clubs.svg'
+    },    
+    
+    clubSeven : {
+        name : "♣7",
+        number : 7,
+        image : './assets/cardSprite/7_of_clubs.svg'
+    },    
+    
+    clubEight : {
+        name : "♣8",
+        number : 8,
+        image : './assets/cardSprite/8_of_clubs.svg'
+    },    
+    
+    clubNine : {
+        name : "♣9",
+        number : 9,
+        image : './assets/cardSprite/9_of_clubs.svg'
+    },    
+    
+    clubTen : {
+        name : "♣10",
+        number : 10,
+        image : './assets/cardSprite/10_of_clubs.svg'
+    },    
+    
+    clubJack : {
+        name : "♣J",
+        number : 10,
+        image : './assets/cardSprite/jack_of_clubs.svg'
+    },
+
+    clubQueen : {
+        name : "♣Q",
+        number : 10,
+        image : './assets/cardSprite/queen_of_clubs.svg'
+    },
+
+    clubKing : {
+        name : "♣K",
+        number : 10,
+        image : './assets/cardSprite/king_of_clubs.svg'
+    },
+    
+
+}
+
+export default cards
